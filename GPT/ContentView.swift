612ae68b851a976572dba1632c65a759f3a78fe7
Andrea <emailandreabollato2000@gmail.com>
2025-11-02 19:55:43 +0100

    Initial Commit

---
+++ GPT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GPT
+//
+//  Created by Andrea Bollato on 02/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GPT/GPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPTApp.swift
+//  GPT
+//
+//  Created by Andrea Bollato on 02/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
